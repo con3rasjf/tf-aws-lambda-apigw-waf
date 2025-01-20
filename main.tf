26527c74118a3af9e5918e100b958f2f7d0057ad
Felipe Contreras <felipecontreras@192.168.1.3>
2025-01-19 22:33:51 -0500

    Fix app client

--- main.tf
+++ main.tf
@@ -211,9 +211,8 @@ resource "aws_cognito_user_pool_client" "app_client" {
   name                                 = "app-client"
   user_pool_id                         = aws_cognito_user_pool.user_pool.id
   generate_secret                      = true
-  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows_user_pool_client = false
   allowed_oauth_flows                  = ["client_credentials"]
-  allowed_oauth_scopes                 = ["phone", "email"]
   supported_identity_providers         = ["COGNITO"]
 
   access_token_validity  = 60
