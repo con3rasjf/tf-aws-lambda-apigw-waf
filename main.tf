e022adffeddf6e2b1cd7851b20821581b4b8760f
Felipe Contreras <felipecontreras@192.168.1.3>
2025-01-19 22:30:33 -0500

    Add App Client and Cognito domain

--- main.tf
+++ main.tf
@@ -210,7 +210,7 @@ resource "aws_cognito_user_pool" "user_pool" {
 resource "aws_cognito_user_pool_client" "app_client" {
   name                                 = "app-client"
   user_pool_id                         = aws_cognito_user_pool.user_pool.id
-  generate_secret                      = false
+  generate_secret                      = true
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = ["client_credentials"]
   allowed_oauth_scopes                 = ["phone", "email", "openid", "profile", "aws.cognito.signin.user.admin"]
